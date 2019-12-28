29884bf2dd2bb6919dc09d3a6737320b8813e0cd
izazasif <asifizaz5@gmail.com>
2019-12-28 21:51:47 +0600

    Add All Files

---
+++ app/Http/Controllers/controller/country.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\controller;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\CountryModel;
+class country extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(CountryModel::get(),200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $country = CountryModel::create($request->all());
+        return response()->json($country,201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $country= CountryModel::find($id);
+        if(is_null($country)){
+       return response()->json('Record Not Found',404);
+        }
+        return response()->json($country,200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $country = CountryModel::findOrFail($id);
+   
+        $country->update($request->all());
+        return response()->json($country,200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $country = CountryModel::findOrFail($id);
+    $country->delete();
+    return response()->json(null,204);
+    }
+}
